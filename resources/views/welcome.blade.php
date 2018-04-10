ab96a64ef9a6f1bc590988439599f7051a0c9764
ybinbin <893842363@qq.com>
2018-04-10 17:17:43 +0800

    jj

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    OJBK
                 </div>
 
                 <div class="links">
